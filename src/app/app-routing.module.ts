2d40a7dbd10bb168934dd75bfc9c0803f14e4649
Raoua-blh <rawaa.blh@gmail.com>
2023-04-17 00:51:46 +0100

    exercice1up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"emploi",component:OffresEmploiComponentComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    FormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/offres-emploi-component/offres-emploi-component.component.ts
+++ src/app/offres-emploi-component/offres-emploi-component.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
 import { Emploi } from '../models/Emploi';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-offres-emploi-component',
@@ -7,29 +8,46 @@ import { Emploi } from '../models/Emploi';
   styleUrls: ['./offres-emploi-component.component.css']
 })
 export class OffresEmploiComponentComponent {
-listeEmploi:Emploi []=[
-  { reference :"01",
-    titre:"Coach ", 
-    entreprise:"EY",
-    etat:true}
- 
-  ];
 
-    public OffreDispo(): number {
-  let nbroffreDispo = 0;
-  for (let i = 0; i < this.listeEmploi.length; i++) {
-    if (this.listeEmploi[i].etat) {
-      nbroffreDispo++;
-    }
+  listeEmploi:Emploi []=[
+    { reference :"01",
+      titre:"Coach ", 
+      entreprise:"EY",
+      etat:true},
+      { reference: "2", titre: 'Ingénieur logiciel',entreprise: 'Microsoft',   etat: false },
+      { reference: "3",  titre: 'Designer UI/UX',entreprise: 'Apple',  etat: true },
+      { reference:"4" ,  titre: 'Analyste de données',entreprise: 'Amazon',  etat: false },
+    
+    ];
+    emploifilter!:string;
+    ngOnInit(): void {
+    this.getEmplois();
   }
-  return nbroffreDispo;
-}
-filteredListeEmploi: Emploi[] = [];
-searchValue = '';
+  getEmplois(): void {
+    this.listeEmploi;
+  }
+     nbroffreDispo = 0;
+      public OffreDispo(): number {
+        this.nbroffreDispo = 0; // reset the value to 0 before counting
 
-filterEmploi() {
-  this.filteredListeEmploi = this.listeEmploi.filter(
-    (emploi) => emploi.entreprise.toLowerCase().includes(this.searchValue.toLowerCase())
-  );
-}
+    for (let i = 0; i < this.listeEmploi.length; i++) {
+      
+      if (this.listeEmploi[i].etat) {
+      
+        this.nbroffreDispo++;
+      }
+    }
+    return this.nbroffreDispo;
+  }
+
+  searchValue :string = '';
+
+  filterEmploi() {
+    let filteredEmploi = this.listeEmploi.filter((listeEmploi) => {
+      return listeEmploi.entreprise.toLowerCase().includes(this.searchValue.toLowerCase());
+    });
+
+    return filteredEmploi;
+  }
+  
 }
